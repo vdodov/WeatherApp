92a62b8f5c8157387cad6c387f814120b6b79f8b
chasuyeon <sy9600@naver.com>
2019-09-21 17:06:39 +0900

    프로젝트 생성 및 appKey 등록

---
+++ Forecast/Forecast/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  Forecast
+//
+//  Created by 차수연 on 21/09/2019.
+//  Copyright © 2019 차수연. All rights reserved.
+//
+
+import Foundation
+
+var appKey = "74019978-9f29-4677-97d5-30c9fbd57daa"
---
+++ Forecast/Forecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecast
+//
+//  Created by 차수연 on 21/09/2019.
+//  Copyright © 2019 차수연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
