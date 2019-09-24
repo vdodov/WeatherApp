c19f1bd1db0b216a62c3a2e0dfe1fda622de47c7
chasuyeon <sy9600@naver.com>
2019-09-25 01:39:14 +0900

    테이블뷰 데이터 소스 구현

--- Forecast/Forecast/ViewController.swift
+++ Forecast/Forecast/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var listTableView: UITableView!
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,3 +21,33 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch section {
+    case 0:
+      return 1
+    case 1:
+      return 0
+    default:
+      return 0
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    if indexPath.section == 0 {
+      let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath)
+      //...
+      
+      return cell
+    }
+    let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath)
+    //...
+    
+    return cell
+  }
+  
+}
